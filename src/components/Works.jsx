9b8ded6eabd3eac15b0c184479f4d7239f3dda57
Paul <eeddll1505@gmail.com>
2023-08-11 23:38:07 +0300

    added tooltip

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,7 +17,16 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn('up', 'sring', index * 0.5, 0.75)}>
-      test
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px["></div>
+      </Tilt>
     </motion.div>
   );
 };
