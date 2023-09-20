16a614ca319c781c165efeb4fb503b715f567303
Paul <eeddll1505@gmail.com>
2023-09-21 00:07:33 +0300

    changes in works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -96,8 +96,7 @@ const Works = () => {
           and manage projects effectively.
         </motion.p>
       </div>
-      <div></div>
-      <div className="mt-10 flex flex-wrap gap-7 ">
+      <div className="mt-20 flex flex-wrap gap-7 ">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} {...project} />
         ))}
